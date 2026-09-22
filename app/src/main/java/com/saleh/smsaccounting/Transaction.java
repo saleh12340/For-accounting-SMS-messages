7ce693db836ac2401e705e89bbb0a9c3ff2ab501
saleh12340 <salthbiany@gmail.com>
2026-09-22 23:38:32 +0300

    feat: mark transaction source

--- app/src/main/java/com/saleh/smsaccounting/Transaction.java
+++ app/src/main/java/com/saleh/smsaccounting/Transaction.java
@@ -1,5 +1,6 @@
 package com.saleh.smsaccounting;
-public class Transaction {
- public long id; public String type, party, reference, body; public double amount, smsBalance, calculatedBalance; public long time;
- public Transaction(long id,String type,String party,String reference,String body,double amount,double smsBalance,long time){this.id=id;this.type=type;this.party=party;this.reference=reference;this.body=body;this.amount=amount;this.smsBalance=smsBalance;this.time=time;}
+public class Transaction{
+ public long id,time;public String type,party,reference,body,source;public double amount,smsBalance,calculatedBalance;
+ public Transaction(long id,String type,String party,String reference,String body,double amount,double smsBalance,long time){this(id,type,party,reference,body,amount,smsBalance,time,"SMS");}
+ public Transaction(long id,String type,String party,String reference,String body,double amount,double smsBalance,long time,String source){this.id=id;this.type=type;this.party=party;this.reference=reference;this.body=body;this.amount=amount;this.smsBalance=smsBalance;this.time=time;this.source=source;}
 }
