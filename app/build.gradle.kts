3caf547433e17f6a88d6fd0a0e95b515c365b420
saleh12340 <salthbiany@gmail.com>
2026-09-22 23:32:57 +0300

    feat: configure application module

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application") }
+
+android { namespace="com.saleh.smsaccounting"; compileSdk=35
+    defaultConfig { applicationId="com.saleh.smsaccounting"; minSdk=26; targetSdk=35; versionCode=1; versionName="1.0" }
+}
